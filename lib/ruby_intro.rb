f3473f777fdac4dc8d917af4b1dc2685c12ed89c
Peter Besze <pbesze@gmail.com>
2018-02-02 21:06:07 +0100

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,73 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum array
+       y = 0
+        array.each do |x|
+          y = y+x
+        end
+  return y
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+   arr = arr.sort.reverse
+   return 0+arr[0].to_i+arr[1].to_i
+
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? (arr, n)
+     y=false
+    carr = arr.combination(2).to_a
+    carr.each do |x|   
+        
+        if  x[0].to_i+x[1].to_i == n
+            y= true
+        end
+    end
+        return y
 end
 
 # Part 2
 
-def hello(name)
-  # YOUR CODE HERE
+def hello name
+  return "Hello, #{name}".to_s
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+   return ( (s =~ /\A[^AEIOU]/i)and (s =~ /\A[a-z]/i) ) == 0
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4? (s)
+   s.to_i(2).round(2) /4 ==s.to_i(2)/4 && s !~ /[^01]/ && s !=''
 end
 
 # Part 3
 
-class BookInStock
-# YOUR CODE HERE
+class BookInStock 
+    def initialize (isbn, price)
+        if price <=0 then raise ArgumentError
+        end
+        if isbn.length <1 then raise ArgumentError
+        end
+    @isbn = isbn
+    @price = price
+    end
+   
+    def isbn
+       @isbn 
+    end
+    def isbn=(isbn)
+        @isbn=isbn
+    end
+    def price
+        @price
+    end
+    def price=(price)
+        @price=price
+    end
+    def price_as_string
+        
+        return '$'+ ("%.2f" % @price).to_s
+    end
+    
 end
